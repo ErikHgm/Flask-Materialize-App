cbced3eea2dd00c01ace59f8c55107b47220bd6f
Erik <erikhgm@gmail.com>
2022-08-24 20:49:51 +0000

    Update tasks template and route

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -15,4 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // select initialization
   let selects = document.querySelectorAll("select");
   M.FormSelect.init(selects);
+
+
+  let collapsible = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsible);
 });
