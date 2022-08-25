e41b842c3b8956c953d1b76ebf5fa0789cfc8371
Erik <erikhgm@gmail.com>
2022-08-25 19:12:38 +0000

    Create edit_task and update routes

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -12,13 +12,15 @@ class Category(db.Model):
 
 class Task(db.Model):
     # schema for the Task model
-    id = db.Column(db.Integer, primary_key= True)
-    task_name = db.Column(db.String(50), unique= True, nullable= False)
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
-    
+
     def __repr__(self):
-        return "#{0} - Task:{1} | Urgemt: {2}".format(
-            self.id, self.task_name, self.is_urgent)
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
